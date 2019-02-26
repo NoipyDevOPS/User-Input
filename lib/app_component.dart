262df9fd13443cd98430f52d26bbc20bbbad365e
Yintong <zhouyintong96@gmail.com>
2019-02-26 13:54:49 +0100

    add imports

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,9 @@
 import 'package:angular/angular.dart';
 import 'src/little_tour_component.dart';
+import 'src/loop_back_component.dart';
+import 'src/click_me_component.dart';
+import 'src/click_me2_component.dart';
+import 'src/keyup_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
